7f6a29c21d6f941b0cf0188afc33b973d623739a
subroto suvro <47747335+suvro1999@users.noreply.github.com>
2022-08-24 02:16:02 +0600

    Loop and condition are added as a new file

---
+++ day2.py
@@ -0,0 +1,20 @@
+#Let's today start with if else
+
+
+a = 10;
+b = 21;
+if a > b:
+    print ("a is gatter than b");
+
+if a < b:
+    print ("a is smaller than b");
+#There are output will be "a is smaller than b".
+
+#yes you can't see any output here because there a is less than b so output not showing
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
